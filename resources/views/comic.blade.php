7af2e9ce41567fb4a779683bb3086dfeea68243a
DonCode <99022919+DonCode86@users.noreply.github.com>
2022-07-10 13:30:35 +0200

    stampa dati comic page

--- resources/views/comic.blade.php
+++ resources/views/comic.blade.php
@@ -5,6 +5,42 @@
 @endsection
  
 @section('page-content')
-  {{-- @include('partials.card') --}}
-  <h1>{{$comic['title']}}</h1>
+  <main>
+    <section class="comic">
+      <div class="comic__cover">
+        <div class="container">
+          <div class="comic__cover-container">
+            <img class="comic__cover-img" src="{{$comic["thumb"]}}" alt="{{$comic["title"]}}">
+            <div class="comic__cover-text">
+              <small>{{$comic["type"]}}</small>
+            </div>
+            <div class="comic__cover-bottom-text">
+              <small>VIEW GALLERY</small>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="flex">
+        <div>
+          <div>
+            <h2>{{$comic['title']}}</h2> 
+          </div>   
+          <div>
+            <div>
+              <span>US. Price <span>{{$comic['price']}}</span></span>
+            </div>   
+            <div>
+              <span>Chech Avialibility</span>
+            </div>
+          </div>
+          <p>
+            {{$comic['description']}}
+          </p>
+        </div>
+        <div>
+          <img src="{{asset('img/adv.jpg')}}" alt="">
+        </div>
+      </div>   
+    </section>
+  </main>
 @endsection
